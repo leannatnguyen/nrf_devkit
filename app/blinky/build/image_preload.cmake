fb70f48953dd01395d9b9affb7f48cda9b1b7f51
Leanna Nguyen <leanna.t.nguyen@gmail.com>
2025-07-25 13:33:20 -0700

    blinky app added to test

---
+++ app/blinky/build/image_preload.cmake
@@ -0,0 +1,6 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52dk_nrf52832" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "/Users/leannanguyen/nrf_devkit/app/blinky/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "/Users/leannanguyen/.local/pipx/venvs/west/bin/python" CACHE INTERNAL "NCS child image controlled")
---
+++ app/blinky/build/zephyr/zephyr.dts.d
@@ -0,0 +1,22 @@
+empty_file.o: /Users/leannanguyen/nrf_devkit/ncs/zephyr/misc/empty_file.c \
+ /Users/leannanguyen/nrf_devkit/ncs/zephyr/boards/arm/nrf52dk_nrf52832/nrf52dk_nrf52832.dts \
+ /Users/leannanguyen/nrf_devkit/ncs/zephyr/dts/arm/nordic/nrf52832_qfaa.dtsi \
+ /Users/leannanguyen/nrf_devkit/ncs/zephyr/dts/common/mem.h \
+ /Users/leannanguyen/nrf_devkit/ncs/zephyr/dts/arm/nordic/nrf52832.dtsi \
+ /Users/leannanguyen/nrf_devkit/ncs/zephyr/dts/arm/armv7-m.dtsi \
+ /Users/leannanguyen/nrf_devkit/ncs/zephyr/dts/common/skeleton.dtsi \
+ /Users/leannanguyen/nrf_devkit/ncs/zephyr/dts/arm/nordic/nrf_common.dtsi \
+ /Users/leannanguyen/nrf_devkit/ncs/zephyr/include/zephyr/dt-bindings/adc/adc.h \
+ /Users/leannanguyen/nrf_devkit/ncs/zephyr/include/zephyr/dt-bindings/adc/nrf-adc.h \
+ /Users/leannanguyen/nrf_devkit/ncs/zephyr/include/zephyr/dt-bindings/dt-util.h \
+ /Users/leannanguyen/nrf_devkit/ncs/zephyr/include/zephyr/sys/util_macro.h \
+ /Users/leannanguyen/nrf_devkit/ncs/zephyr/include/zephyr/sys/util_internal.h \
+ /Users/leannanguyen/nrf_devkit/ncs/zephyr/include/zephyr/sys/util_loops.h \
+ /Users/leannanguyen/nrf_devkit/ncs/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ /Users/leannanguyen/nrf_devkit/ncs/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
+ /Users/leannanguyen/nrf_devkit/ncs/zephyr/include/zephyr/dt-bindings/pinctrl/nrf-pinctrl.h \
+ /Users/leannanguyen/nrf_devkit/ncs/zephyr/include/zephyr/dt-bindings/pwm/pwm.h \
+ /Users/leannanguyen/nrf_devkit/ncs/zephyr/dts/common/freq.h \
+ /Users/leannanguyen/nrf_devkit/ncs/zephyr/dts/arm/nordic/override.dtsi \
+ /Users/leannanguyen/nrf_devkit/ncs/zephyr/boards/arm/nrf52dk_nrf52832/nrf52dk_nrf52832-pinctrl.dtsi \
+ /Users/leannanguyen/nrf_devkit/ncs/zephyr/include/zephyr/dt-bindings/input/input-event-codes.h
